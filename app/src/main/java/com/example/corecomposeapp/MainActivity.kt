4b431fc2ffd4225b8c898364443d2d8e2dcda6a0
dani <daniellesubject@gmail.com>
2022-02-22 21:05:42 -0500

    learning how to nest rows in columns

--- app/src/main/java/com/example/corecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/corecomposeapp/MainActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.corecomposeapp.ui.theme.CoreComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,23 +27,35 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Row {
-            HorizontalColouredBar(color = Color.Magenta)
-            HorizontalColouredBar(color = Color.Red)
-            HorizontalColouredBar(color = Color.Blue)
-            HorizontalColouredBar(color = Color.Green)
-            HorizontalColouredBar(color = Color.Yellow)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // nesting a Row inside a Column
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ColouredSquare(color = Color.Blue)
+                ColouredSquare(color = Color.Magenta)
+            }
+            // UI function being reused
+            ColouredSquare(color = Color.Red)
+            ColouredSquare(color = Color.Green)
+            ColouredSquare(color = Color.Yellow)
         }
     }
 }
 
+
 @Composable
-fun HorizontalColouredBar(color: Color) {
+fun ColouredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
-            .height(600.dp)
-            .width(60.dp)
+            .height(100.dp)
+            .width(100.dp)
     ) { }
 
 }
